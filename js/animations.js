84d6ecd9a39088dbb8e12a2516dc7aba5b9ed42d
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2020-05-08 00:42:09 -0400

    Finished Landing Animations and Styling Tweaks

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,40 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav_links');
-const links = document.querySelectorAll('.nav_links li');
+// Landing animations for menu and main texts
+// document.addEventListener("DOMContentLoaded", function() {
+//   gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
+//   gsap.from(".hamburger", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.5});
+//   gsap.from(".nav-animation-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.5, stagger: 0.2});
+// });
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.innerWidth > 1024) {
+    desktopLandingAnimation();
+  } else {
+    mobileLandingAnimation();
+  }
+});
 
-hamburger.addEventListener("click", animate_button);
+function desktopLandingAnimation() {
+  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
+  gsap.from("#nav-animation-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.5, stagger: 0.2});
+}
 
-function animate_button() {
-  navLinks.classList.toggle("open");
-};
+function mobileLandingAnimation() {
+  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
+  gsap.from(".hamburger", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.5});
+}
+
+function showMenu() {
+  var navLinks = document.getElementById("nav-links-wrapper");
+  if (navLinks.style.visibility === 'visible') {
+
+    var tl = gsap.timeline({onComplete: function() {navLinks.style.visibility = 'hidden'}});
+    tl.to("#nav-links-wrapper", {duration: 0.5, opacity: 0});
+    
+  } else {
+    navLinks.style.visibility = 'visible';
+    navLinks.style.opacity = 0.0;
+    var tl = gsap.timeline();
+    tl.to("#nav-links-wrapper", {duration: 0.4, opacity: 1})
+    tl.from("#nav-animation-wrapper", {duration: 1, ease: "elastic.out(0.3, 0.5)", y: -100, opacity: 0, stagger: 0.2});
+  }
+  
+}
