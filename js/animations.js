a161b0881c24dffb6287799527c21e76795303b6
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2020-05-11 03:09:38 -0400

    animations optimization

--- js/animations.js
+++ js/animations.js
@@ -8,21 +8,21 @@ document.addEventListener("DOMContentLoaded", function() {
     mobileLandingAnimation();
     gsap.to(".main-text-wrapper", {duration: 5, ease: "elastic.inOut(3, 1)", y:"-20%", stagger: 0.2, repeat: -1, yoyo: true, delay: 5}, 1);
   }
-  gsap.from("#subpage-desc", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
-  gsap.from(".card-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.8)", y: 100, opacity: 0, delay: 0.7, stagger: 0.1});
+  gsap.from("#subpage-desc", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.015, delay: 1, stagger: 0.2});
+  gsap.from(".card-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.8)", y: 100, opacity: 0.015, delay: 1, stagger: 0.2});
 });
 
 
 function desktopLandingAnimation() {
-  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
-  gsap.from("#nav-animation-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.5, stagger: 0.2});
-  gsap.from(".sub-content", {duration: 5, opacity: 0, ease: "elastic.out(0.6, 0.5)", y:-500, delay: 0.5});
+  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.015, delay: 0.5, stagger: 0.2});
+  gsap.from("#nav-animation-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0.015, delay: 0.5, stagger: 0.2});
+  gsap.from(".sub-content", {duration: 5, opacity: 0.015, ease: "elastic.out(0.6, 0.5)", y:-500, delay: 0.5});
 }
 
 function mobileLandingAnimation() {
-  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0, delay: 0.5, stagger: 0.2});
-  gsap.from(".hamburger", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0, delay: 0.2});
-  gsap.from(".sub-content", {duration: 0.5, opacity: 0, delay: 1});
+  gsap.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.015, delay: 0.5, stagger: 0.2});
+  gsap.from(".hamburger", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0.015, delay: 0.2});
+  gsap.from(".sub-content", {duration: 0.5, opacity: 0.015, delay: 1});
 }
 
 // Mobile Menu Animation for Hiding and Revealing Menu List
