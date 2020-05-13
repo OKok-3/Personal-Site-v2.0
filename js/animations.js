d8d70e70f561034bb218525f5eb878743a54008c
OKok-3 <53050459+OKok-3@users.noreply.github.com>
2020-05-13 00:58:13 -0400

    tweaked preloader animations and styling

--- js/animations.js
+++ js/animations.js
@@ -8,13 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener("load", function() {
   if (window.innerWidth >= 1400) {
     gsap.to(".preloader-container", {delay: 0.5, duration: 0.5, opacity: 0, onComplete: desktopLandingAnimation()});
+    // Card Animations
+    var tl_w = gsap.timeline({delay: 1})
+    tl_w.from("#subpage-desc", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.01, stagger: 0.2}, 1);
+    tl_w.from(".card-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.01, stagger: 0.2}, 1.5);
   } else {
     gsap.to(".preloader-container", {delay: 0.5, duration: 0.5, opacity: 0, onComplete: mobileLandingAnimation()});
+    // Card Animations
+    var tl_w = gsap.timeline({delay: 1})
+    tl_w.from("#subpage-desc", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.01, stagger: 0.2}, 1);
+    tl_w.from(".card-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.011, stagger: 0.3}, 1.5);
   }
-  // Card Animations
-  var tl_w = gsap.timeline({delay: 1})
-  tl_w.from("#subpage-desc", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.01, stagger: 0.2}, 1);
-  tl_w.from(".card-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.01, stagger: 0.2}, 1.5);
+  
 });
 
 
@@ -31,7 +36,7 @@ function desktopLandingAnimation() {
 function mobileLandingAnimation() {
   var tl_m = gsap.timeline({delay: 1});
   // Menu Button Animations
-  tl_m.from(".hamburger", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0.015, onComplete: disable_preloader()}, 1);
+  tl_m.from("#mobile-menu", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: -100, opacity: 0.015, stagger: 0.2, onComplete: disable_preloader()}, 1);
   // For index page main texts
   tl_m.from(".main-text-wrapper", {duration: 3, ease: "elastic.out(0.6, 0.5)", y: 100, opacity: 0.015, delay: 0.5, stagger: 0.2}, 1);
   // For sub contents
@@ -47,16 +52,18 @@ function showMenu() {
   var navLinks = document.getElementById("nav-links-wrapper");
   if (navLinks.style.visibility === 'visible') {
     var tl = gsap.timeline({onComplete: function() {navLinks.style.visibility = 'hidden'}});
-    tl.to("#nav-links-wrapper", {duration: 0.3, opacity: 0});
-    tl.to(".sub-content", {duration: 0.3, opacity: 1});
+    tl.to("#nav-links-wrapper", {duration: 0.2, opacity: 0});
+    tl.to(".sub-content", {duration: 0.2, opacity: 1});
     tl.to(".subpage-content-container", {duration: 0.2, opacity: 1}, 1);
+    tl.to(".exp-subpage-content-container", {duration: 0.2, opacity: 1}, 1);
   } else {
     navLinks.style.visibility = 'visible';
     navLinks.style.opacity = 0.0;
     var tl = gsap.timeline();
-    tl.to("#nav-links-wrapper", {duration: 0.3, opacity: 1}, 1)
-    tl.from("#nav-animation-wrapper", {duration: 1, ease: "elastic.out(0.3, 0.5)", y: -100, opacity: 0, stagger: 0.2}, 1);
+    tl.to("#nav-links-wrapper", {duration: 0.2, opacity: 1}, 1)
+    tl.from("#nav-animation-wrapper", {duration: 1, ease: "elastic.out(0.3, 1)", y: -100, opacity: 0, stagger: 0.2}, 1);
     tl.to(".sub-content", {duration: 0.1, opacity: 0}, 1);
     tl.to(".subpage-content-container", {duration: 0.2, opacity: 0}, 1);
+    tl.to(".exp-subpage-content-container", {duration: 0.2, opacity: 0}, 1);
   }
 }
